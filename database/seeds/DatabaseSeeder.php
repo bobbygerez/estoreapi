b3b7de12289a4665cd3111463c0eeb2ea0b74158
Bobby Gerez <bobby.gerez@yahoo.com>
2018-07-09 16:06:10 +0300

    DB Migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Model\User;
+use App\Model\Category;
+use App\Model\SubCategory;
+use App\Model\FurtherCategory;
+use App\Model\Unit;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,9 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        User::truncate();
+        Category::truncate();
+        SubCategory::truncate();
+        FurtherCategory::truncate();
+        Unit::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(SubCategoriesTableSeeder::class);
         $this->call(FurtherCategoriesTableSeeder::class);
+        $this->call(UnitsTableSeeder::class);
     }
 }
